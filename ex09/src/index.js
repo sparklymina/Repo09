fb162317ce2340ca253a4b8c06cb7ba052ea6a2d
sparklymina <ecausevic93@gmail.com>
2021-09-21 15:43:49 -0400

    adding ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function functionElse(num){
+if (num <= 5) {
+    return "5 or smaller";
+} else {
+    return "Bigger than 5";
+}
+}
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+// Only change code above this line
+module.exports = functionElse;
